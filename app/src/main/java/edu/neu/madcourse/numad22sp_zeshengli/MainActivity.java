606548a584a142da1d1fbec7a1d0c5a5d311a762
ZeitLee <li.zes@husky.neu.edu>
2022-02-06 23:58:56 -0500

    Assignment3 v0.5

--- app/src/main/java/edu/neu/madcourse/numad22sp_zeshengli/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_zeshengli/MainActivity.java
@@ -6,15 +6,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView clickTxt;
+    Button bt2;
+    Button bt3;
+    Button bt4;
+    Button bt5;
+    Button bt6;
+    Button bt7;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickTxt = (TextView) findViewById(R.id.textView2);
+        bt2 = (Button) findViewById(R.id.button2);
+        bt3 = (Button) findViewById(R.id.button3);
+        bt4 = (Button) findViewById(R.id.button4);
+        bt5 = (Button) findViewById(R.id.button5);
+        bt6 = (Button) findViewById(R.id.button6);
+        bt7 = (Button) findViewById(R.id.button7);
 
         //This is about me button
         Button about_me_btn = (Button) findViewById(R.id.aboutMe);
@@ -24,6 +41,43 @@ public class MainActivity extends AppCompatActivity {
                 //Log.i("AboutMeMessage", "Name:Zesheng Li\nemail: li.zes@husky.neu.edu.");
                 Toast.makeText(getApplicationContext(), "Name:Zesheng Li\nemail: li.zes@husky" +
                         ".neu.edu.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: A");
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: B");
+            }
+        });
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: C");
+            }
+        });
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: D");
+            }
+        });
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: E");
+            }
+        });
+        bt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickTxt.setText("Pressed: F");
             }
         });
     }
