4afdbf7b22ec32b83b0477c0fe9a747c8bffc596
ZeitLee <li.zes@husky.neu.edu>
2022-02-21 03:42:36 -0500

    Assignment5 fix crashing bug when open locator.

--- app/src/main/java/edu/neu/madcourse/numad22sp_zeshengli/Location.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_zeshengli/Location.java
@@ -8,22 +8,35 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class Location extends AppCompatActivity {
 
     TextView latitude, longitude;
+    Button getLocation;
 
-    // Location manager.
-    private LocationManager locationManager;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,32 +45,51 @@ public class Location extends AppCompatActivity {
 
         latitude = (TextView) findViewById(R.id.latitude);
         longitude = (TextView) findViewById(R.id.longitude);
+        getLocation = (Button) findViewById(R.id.getLocation);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // request permission from user.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-        }
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateLocation();
+            }
+        });
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10,
-                1, new LocationListener() {
-                    @Override
-                    public void onLocationChanged(@NonNull android.location.Location location) {
-                        latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
-                        longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
-                    }
-                });
 
     }
-    // re-write back button to back to main activity.
+
     @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        intent .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 86:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateLocation();
+                }
+                else {
+                    Toast.makeText(Location.this, "No Permission.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+    private  void updateLocation() {
+        // check permission and only check once when no permission.
+        if (ActivityCompat.checkSelfPermission(Location.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull android.location.Location location) {
+                    latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+                    longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+                }
+            });
+        } else {
+            // ask permission.
+            ActivityCompat.requestPermissions(Location.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 86);
+        }
     }
 
 }
